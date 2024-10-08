d1db406b387ad113fc203aa455f2e3b5b5be4d9e
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-08 21:31:00 +0300

    Updated populate for supporting session.

--- db/dbpopulate.mjs
+++ db/dbpopulate.mjs
@@ -32,6 +32,15 @@ const SQL = `
     code VARCHAR(100) NOT NULL
   );
 
+  CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+  )
+  WITH (OIDS=FALSE);
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+  CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
   INSERT INTO club_codes (name, code)
   VALUES
   ('admin', 'admin'),
