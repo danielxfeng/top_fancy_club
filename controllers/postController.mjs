9ed8aea597851c7eb7eef1dc3562b14c48f07ed6
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-08 23:01:28 +0300

    Updated appRouter.

---
+++ controllers/postController.mjs
@@ -0,0 +1,7 @@
+const homeGet = (req, res) => {};
+const postsGet = (req, res) => {};
+const postsNewGet = (req, res) => {};
+const postsNewPost = (req, res) => {};
+const postsDelete = (req, res) => {};
+
+export { homeGet, postsGet, postsNewGet, postsNewPost, postsDelete };
---
+++ routes/appRouter.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  homeGet,
+  postsGet,
+  postsNewGet,
+  postsNewPost,
+  postsDelete,
+} from "../controllers/postController.mjs";
+
+const appRouter = express.Router();
+
+appRouter.get("/", homeGet);
+appRouter.get("/posts", postsGet);
+appRouter.get("/posts/new", postsNewGet);
+appRouter.post("/posts/new", postsNewPost);
+appRouter.delete("/posts/:id", postsDelete);
